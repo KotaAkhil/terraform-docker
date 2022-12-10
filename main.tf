8d2908160c0430048215e754610cfe2fa23b3ea1
KotaAkhil <117889604+KotaAkhil@users.noreply.github.com>
2022-12-10 10:33:00 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,3 +6,23 @@ terraform {
     }
   }
 }
+provider "docker" {
+  # Configuration options
+  host = "unix:///var/run/docker.sock"
+}
+
+resource "docker_image" "ubuntu" {
+  name = var.ubuntu_image
+}
+
+resource "docker_container" "ubuntu" {
+  name     = var.container_name
+  image    = docker_image.ubuntu.image_id
+  must_run = true
+  command = [
+    "tail",
+    "-f",
+    "/dev/null"
+  ]
+}
+
